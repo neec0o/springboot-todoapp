b3456877da06589ef5a1f2c20d5582848eafff3b
Nico <bua-nico@hotmail.de>
2025-06-11 20:34:45 +0200

    Init Commit

---
+++ todoapp-backend/src/main/java/de/devnico/todoappbackend/TodoappBackendApplication.java
@@ -0,0 +1,13 @@
+package de.devnico.todoappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappBackendApplication.class, args);
+    }
+
+}
---
+++ todoapp-backend/src/test/java/de/devnico/todoappbackend/TodoappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.devnico.todoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
